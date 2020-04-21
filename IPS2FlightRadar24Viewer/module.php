2c46a4a85e5f7cc8482d8d22f9de60f1ba27d42b
Joey-1970 <paeper@horburg.de>
2020-04-21 10:43:50 +0200

    Update module.php

--- IPS2FlightRadar24Viewer/module.php
+++ IPS2FlightRadar24Viewer/module.php
@@ -92,7 +92,7 @@
             			$this->SendDebug("Statistics", "Datei ist kein Array!", 0);
         		}
         		else {
-            			$StatisticArray = array("last1min" => "Minute", "last5min" => "5 Minuten", "last15min" => "15 Minuten", "total" => "Insgesamt");
+            			$StatisticArray = array("last1min" => "1 Minute", "last5min" => "5 Minuten", "last15min" => "15 Minuten", "total" => "Insgesamt");
         			
 				// HTML Tabelle erstellen
             			$HTML = "<table border='1'>";

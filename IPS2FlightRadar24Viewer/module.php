ff142f0b2bd4c665e004618dc69f4df1748a2482
Joey-1970 <paeper@horburg.de>
2020-04-21 12:04:43 +0200

    Update module.php

--- IPS2FlightRadar24Viewer/module.php
+++ IPS2FlightRadar24Viewer/module.php
@@ -99,7 +99,7 @@
             
 				$HTML .= "<thead>";
 				$HTML .= "<tr>";
-				$Headline = "Stand: ".date('d.m.Y H:i', $Statistics->latest->end);
+				$Headline = "Stand:<br>".date('d.m.Y H:i', $Statistics->latest->end);
 				$HTML .= "<th>$Headline</th>";
 				$HTML .= "<th>Start</th>";
 				$HTML .= "<th>Samples<br>durchgeführt</th>";

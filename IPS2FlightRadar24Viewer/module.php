a1407e6f4bbc317b34ae1e7d042d527ba022d10f
Joey-1970 <paeper@horburg.de>
2020-05-05 21:14:02 +0200

    Update module.php

--- IPS2FlightRadar24Viewer/module.php
+++ IPS2FlightRadar24Viewer/module.php
@@ -363,7 +363,7 @@
 			foreach ($DataArray[$SessionID] as $AircraftID => $Value) {
 				$HTML .= "<tbody>";
 				If ((is_numeric($DataArray[$SessionID][$AircraftID]["Latitude"])) AND (is_numeric($DataArray[$SessionID][$AircraftID]["Longitude"]))) {
-					$HTML .= "<tr bgcolor=#A9F5A9>";
+					$HTML .= "<tr bgcolor=#088A08>";
 				}
 				else {
 					$HTML .= "<tr>";
